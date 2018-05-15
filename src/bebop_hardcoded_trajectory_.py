5d282d85ea67b1a3b682ec0634f3b8c84c261536
Suhail Pallath Sulaiman <suhailsulaiman2018@u.northwestern.edu>
2018-05-15 17:12:10 -0500

    Added publisher for resetting

--- src/bebop_hardcoded_trajectory_.py
+++ src/bebop_hardcoded_trajectory_.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
-#import library ros 
-import rospy 
+#import library ros
+import rospy
 import time
 import sys
 import math
-#import library untuk mengirim command dan menerima data navigasi dari quadcopter
+
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String 
-from std_msgs.msg import Empty 
+from std_msgs.msg import String
+from std_msgs.msg import Empty
 from std_msgs.msg import UInt8
 #from ardrone_autonomy.msg import Navdata
 
-#import class status untuk menentukan status ddari quadcopter
+#import class status
 #from drone_status import DroneStatus
 
 COMMAND_PERIOD = 1000
@@ -27,25 +27,26 @@ class AutonomousFlight():
         self.pubLand = rospy.Publisher("bebop/land",Empty, queue_size=10)
         self.pubCommand = rospy.Publisher('bebop/cmd_vel',Twist, queue_size=10)
         self.pubFlip = rospy.Publisher('bebop/flip',UInt8, queue_size=10)
+        self.reset=rospy.Publisher("bebop/reset",Empty, queue_size=10)
         self.command = Twist()
         #self.commandTimer = rospy.Timer(rospy.Duration(COMMAND_PERIOD/1000.0),self.SendCommand)
-        self.state_change_time = rospy.Time.now()    
+        self.state_change_time = rospy.Time.now()
         rospy.on_shutdown(self.SendLand)
 
     def SendTakeOff(self):
         self.pubTakeoff.publish(Empty())
         print("takeoff")
         self.rate.sleep()
-                
+
     def SendLand(self):
         self.pubLand.publish(Empty())
         print("landing")
-		
-    
+
+
     def FlipMe(self, data): # Only for Bebop; 0 - flip forward, 1 - flip backward, 2 - flip right, 3 - flip left
         self.pubFlip.publish(UInt8(data))
-   
-		       
+
+
     def SetCommand(self, linear_x, linear_y, linear_z, angular_x, angular_y, angular_z):
         self.command.linear.x = linear_x #Forward & Backward
         self.command.linear.y = linear_y #Left (+) & Right (-)
@@ -87,14 +88,14 @@ class AutonomousFlight():
 		for i in range(int(10 * t)):
 			self.SetCommand(0,0,1,0,0,0)
 			print("Go backward" + str(i))
-    
+
     def goRight(self, s):
 		v = 1
 		t = s / v
 		for i in range(int(10 * t)):
 			self.SetCommand(0,-1,0,0,0,0)
-			print("Go backward" + str(i))   
-  
+			print("Go backward" + str(i))
+
     def Turnright(self, deg):
 		rad = deg * (math.pi / 180)
 		t = int(10*rad)
@@ -117,7 +118,7 @@ class AutonomousFlight():
 		for i in range(int(t * 10/2)):
 			self.SetCommand(v,0,0.1,0,0,w)
 			print("spiralling " + str(i))
-			
+
     def circle(self, r, t): # testing a small circle
 		circum = 2 * math.pi * r
 		v = circum / t
@@ -125,16 +126,16 @@ class AutonomousFlight():
 		for i in range(int(t * 10)):
 			self.SetCommand(v,0,0,0,0,w)
 			print("circle " + str(i))
-						
 
 
 
 
-if __name__ == '__main__': 
-    try: 
-        
+
+if __name__ == '__main__':
+    try:
+
         uav = AutonomousFlight()
-        count = 0    
+        count = 0
         while not rospy.is_shutdown():
 			i = 0
 			uav.SendTakeOff()
@@ -147,8 +148,9 @@ if __name__ == '__main__':
 			count+=1
 
 
-    	
-		
-         
+
+
+
     except rospy.ROSInterruptException:
+        self.reset.publish(Empty())
         pass
